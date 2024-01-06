0681e93d9afdf2a117b5b2b0ee30082f76fab1e0
zander-raycraft <zanderraycraft504@gmail.com>
2024-01-06 11:29:39 -0600

    included React Router

--- finace/src/App.tsx
+++ finace/src/App.tsx
@@ -1,111 +1,15 @@
-import React, { useState, ChangeEvent } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './pages/landing';
 
-function App() {
-  const [usernameValue, setUsernameValue] = useState('');
-  const [nameValue, setNameValue] = useState('');
-  const [passwordValue, setPasswordValue] = useState('');
-  const [passwordconfValue, setPasswordConfValue] = useState('');
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const [passwordConfVisible, setPasswordConfVisible] = useState(false);
-  const [passwordsMatch, setPasswordsMatch] = useState(false);
-
-  const handleInputChange = (propertyName: string) => (
-    event: ChangeEvent<HTMLInputElement>
-  ) => {
-    switch (propertyName) {
-      case 'username':
-        setUsernameValue(event.target.value);
-        break;
-      case 'name':
-        setNameValue(event.target.value);
-        break;
-      case 'password':
-        setPasswordValue(event.target.value);
-        break;
-      case 'passwordconf':
-        setPasswordConfValue(event.target.value);
-        break;
-      default:
-        break;
-    }
-
-    if (propertyName === 'password' || propertyName === 'passwordconf') {
-      setPasswordsMatch(passwordValue === passwordconfValue);
-    }
-  };
-
-  const isPasswordEmpty = passwordValue.trim() === '';
-  const isPasswordConfEmpty = passwordconfValue.trim() === '';
-
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <div className='coin-container'>
-        <img src={process.env.PUBLIC_URL + '/coinPot2.png'} alt='coin-pot' />
-      </div>
-      <div className="login-container">
-        <div className='sign-up-container'>
-          <h1>Create account</h1>
-          <h2>Sign up to start tracking finances</h2>
-        </div>
-        <div className='user-info-container'>
-
-          {/* Username information */}
-          <input
-            type="text"
-            value={usernameValue}
-            onChange={handleInputChange('username')}
-            placeholder="Enter your username"
-          />
-
-          {/* Name information */}
-          <input
-            type="text"
-            value={nameValue}
-            onChange={handleInputChange('name')}
-            placeholder="Enter your name"
-          />
-
-          {/* Password information */}
-          <div className="input-container">
-            <input
-              type={passwordVisible ? 'text' : 'password'}
-              value={passwordValue}
-              onChange={handleInputChange('password')}
-              placeholder="Enter your password"
-            />
-            {isPasswordEmpty ? null : (
-              <button
-                className="toggle-password"
-                onClick={() => setPasswordVisible(!passwordVisible)}
-              >
-                {passwordVisible ? 'Hide' : 'Show'}
-              </button>
-            )}
-          </div>
-
-          {/* Password verification information */}
-          <div className="input-container">
-            <input
-              type={passwordConfVisible ? 'text' : 'password'}
-              value={passwordconfValue}
-              onChange={handleInputChange('passwordconf')}
-              placeholder="Confirm password"
-            />
-            {isPasswordConfEmpty ? null : (
-              <button
-                className="toggle-password"
-                onClick={() => setPasswordConfVisible(!passwordConfVisible)}
-              >
-                {passwordConfVisible ? 'Hide' : 'Show'}
-              </button>
-            )}
-          </div>
-        </div>
-        <div className='login-page-container'></div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ finace/src/pages/landing.tsx
@@ -0,0 +1,115 @@
+import React, { useState, ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/App.css';
+
+function Landing() {
+  const [usernameValue, setUsernameValue] = useState('');
+  const [nameValue, setNameValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [passwordconfValue, setPasswordConfValue] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordConfVisible, setPasswordConfVisible] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(false);
+
+  const handleInputChange = (propertyName: string) => (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    switch (propertyName) {
+      case 'username':
+        setUsernameValue(event.target.value);
+        break;
+      case 'name':
+        setNameValue(event.target.value);
+        break;
+      case 'password':
+        setPasswordValue(event.target.value);
+        break;
+      case 'passwordconf':
+        setPasswordConfValue(event.target.value);
+        break;
+      default:
+        break;
+    }
+
+    if (propertyName === 'password' || propertyName === 'passwordconf') {
+      setPasswordsMatch(passwordValue === passwordconfValue);
+    }
+  };
+
+  const isPasswordEmpty = passwordValue.trim() === '';
+  const isPasswordConfEmpty = passwordconfValue.trim() === '';
+
+  return (
+    <div className="App">
+      <div className='coin-container'>
+        <img src={process.env.PUBLIC_URL + '/coinPot2.png'} alt='coin-pot' />
+      </div>
+      <div className="login-container">
+        <div className='sign-up-container'>
+          <h1>Create account</h1>
+          <h2>Sign up to start tracking finances</h2>
+        </div>
+        <div className='user-info-container'>
+
+          {/* Username information */}
+          <input
+            type="text"
+            value={usernameValue}
+            onChange={handleInputChange('username')}
+            placeholder="Enter your username"
+          />
+
+          {/* Name information */}
+          <input
+            type="text"
+            value={nameValue}
+            onChange={handleInputChange('name')}
+            placeholder="Enter your name"
+          />
+
+          {/* Password information */}
+          <div className="input-container">
+            <input
+              type={passwordVisible ? 'text' : 'password'}
+              value={passwordValue}
+              onChange={handleInputChange('password')}
+              placeholder="Enter your password"
+            />
+            {isPasswordEmpty ? null : (
+              <button
+                className="toggle-password"
+                onClick={() => setPasswordVisible(!passwordVisible)}
+              >
+                {passwordVisible ? 'Hide' : 'Show'}
+              </button>
+            )}
+          </div>
+
+          {/* Password verification information */}
+          <div className="input-container">
+            <input
+              type={passwordConfVisible ? 'text' : 'password'}
+              value={passwordconfValue}
+              onChange={handleInputChange('passwordconf')}
+              placeholder="Confirm password"
+            />
+            {isPasswordConfEmpty ? null : (
+              <button
+                className="toggle-password"
+                onClick={() => setPasswordConfVisible(!passwordConfVisible)}
+              >
+                {passwordConfVisible ? 'Hide' : 'Show'}
+              </button>
+            )}
+          </div>
+        </div>
+        <div className='login-page-container'>
+          <h3> Already have an account? <Link to="/login">Login</Link>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
